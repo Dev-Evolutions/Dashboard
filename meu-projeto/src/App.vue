12ea9aaf1b0237c922e5acd6aacc05b3a1157ce6
enricodelguerra <ricodelguerra@gmail.com>
2024-10-07 09:14:48 -0300

    Tailwind instalado e login com credenciais terminado

--- meu-projeto/src/App.vue
+++ meu-projeto/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-
+  <div id="app">
+    <UserLogin />
+  </div>
 </template>
 
 <script>
+import UserLogin from './Components/UserLogin.vue';
 
+export default {
+  components: {
+    UserLogin
+  }
+}
 </script>
 
 <style></style>
---
+++ meu-projeto/src/Components/UserLogin.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="login container mx-auto py-8" v-if="!isAuthenticated">
+        <h1 class="text-3xl font-bold text-center mb-6">Login</h1>
+        <form class="max-w-md mx-auto" @submit.prevent="login">
+            <div class="mb-4">
+                <label for="username" class="block text-gray-700">Usuário:</label>
+                <input type="text" id="username" v-model="username"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-md" />
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700">Senha:</label>
+                <input type="password" id="password" v-model="password"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-md" />
+            </div>
+            <button type="submit" class="w-full bg-red-950 text-white py-2 px-4 rounded-md hover:bg-green-700">
+                Entrar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            isAuthenticated: false
+        };
+    },
+    methods: {
+        login() {
+            if (this.username === 'admin' && this.password === '1234') {
+                this.isAuthenticated = true;
+            } else {
+                alert('Credenciais inválidas.');
+            }
+        }
+    }
+    
+};
+</script>
