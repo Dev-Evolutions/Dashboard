91a751d54213292834b2c97f37bc7f0ef34e659e
andrealtobelli <andrerogerio03@gmail.com>
2024-10-07 09:32:33 -0300

    adicionado cabeçalho ao dashboard financeiro

---
+++ meu-projeto/src/components/Dashboard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <header>
+      <h1>Dashboard Financeiro - Exprezzo Cafeteria</h1>
+      <img src="@/assets/logo_cafe_com_escrita_exprezzo.png" alt="Logo da Exprezzo Cafeteria"/>
+    </header>
+</template>
