e0ff8e6776c71b429ff3bc90ead3225ee7ac5524
andrealtobelli <andrerogerio03@gmail.com>
2024-10-07 09:34:51 -0300

    adicionada tabela de entradas e saídas

--- meu-projeto/src/components/Dashboard.vue
+++ meu-projeto/src/components/Dashboard.vue
@@ -1,7 +1,34 @@
 <template>
   <div>
     <header>
-      <h1>Dashboard Financeiro - Exprezzo Cafeteria</h1>
-      <img src="@/assets/logo_cafe_com_escrita_exprezzo.png" alt="Logo da Exprezzo Cafeteria"/>
+    <h1>Dashboard Financeiro - Exprezzo Cafeteria</h1>
+    <img src="@/assets/logo_cafe_com_escrita_exprezzo.png" alt="Logo da Exprezzo Cafeteria"/>
     </header>
+        <main>
+            <table>
+                <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Tipo</th>
+                    <th>Quantidade</th> 
+                    <th>Valor (R$)</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(entrada, index) in entradas" :key="index">
+                    <td>{{ entrada.descricao }}</td>
+                    <td>Entrada</td>
+                    <td>{{ entrada.quantidade }}</td> 
+                    <td>{{ entrada.valor }}</td>
+                </tr>
+                <tr v-for="(saida, index) in saidas" :key="index">
+                    <td>{{ saida.descricao }}</td>
+                    <td>Saída</td>
+                    <td>{{ saida.quantidade }}</td> 
+                    <td>{{ saida.valor }}</td>
+                </tr>
+                </tbody>
+            </table>
+            </main>
+    </div>
 </template>
