5ccd27179c600d13a97228f89dfffbce6689efe9
fernandohirashima <fernando.hitoshi@hotmail.com.br>
2024-10-08 09:32:23 -0300

    Ajuste da estrutura de Login e alteração de cor

--- meu-projeto/src/Components/FinancialDashboard.vue
+++ meu-projeto/src/Components/FinancialDashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <header>
       <h1>Dashboard Financeiro - Exprezzo Cafeteria</h1>
-      <img src="@/assets/logo_cafe_com_escrita_exprezzo.png" alt="Logo da Exprezzo Cafeteria"/>
+      <img src="" alt="Logo da Exprezzo Cafeteria"/>
     </header>
 
     <main>
